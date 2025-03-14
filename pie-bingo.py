76c2b1bb45e5c6bb63981af2ede27f679c64da13
Abhay Gupta <ab18gu@gmail.com>
2025-03-14 16:05:03 -0700

    combine pies horizontally

--- pie-bingo.py
+++ pie-bingo.py
@@ -7,12 +7,14 @@
 
 import os
 from PIL import Image
+import sys
 
 def main():
     """ runs pie generator """
 
     directory = "./images/pies/"
     pies = []
+    imgs = []
 
     # add images to an array
     for entry in os.scandir(directory):
@@ -20,6 +22,8 @@ def main():
             with Image.open(entry) as img:
                 print(entry.path)
                 pies.append(entry)
+                imgs.append(img)
+
         except (IOError, SyntaxError):
             pass
 
@@ -31,6 +35,23 @@ def main():
 
         #img = Image.open(pie)
         #img.show()
+
+    images = [Image.open(x) for x in pies]
+
+    widths, heights = zip(*(i.size for i in images))
+
+    total_width = sum(widths)
+    max_height = max(heights)
+
+    new_im = Image.new('RGB', (total_width, max_height))
+
+    x_offset = 0
+    for im in images:
+      new_im.paste(im, (x_offset,0))
+      x_offset += im.size[0]
+
+    new_im.save('test.jpg')
+
 
 
 
